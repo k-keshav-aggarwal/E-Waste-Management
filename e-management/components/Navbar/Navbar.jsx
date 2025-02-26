147d3f7505f2be18e0b9e8cffa21916a3d46568a
Bluhawk2004 <161112797+CholarajuAditya@users.noreply.github.com>
2025-02-26 13:25:20 +0530

    responsive navbar done

--- e-management/components/Navbar/Navbar.jsx
+++ e-management/components/Navbar/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState } from "react"
+import { BiMenu, BiX } from "react-icons/bi";
+import "./Navbar.css"
 
-const Navbar = () => {
+const Navbar  = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleNavbar = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div>Hello Navbar</div>
+      <header>
+        <h1>Logo</h1>
+        <div className={`navbar-container ${isOpen ? "active" : ""}`}>
+          <BiX className="navbar-close-icon" onClick={handleNavbar}/>
+          <nav className="nav-links-1">
+            <ul className="link-style">
+              <li><a href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </nav>
+          <button className="login-btn-1">login</button>
+        </div>
+        <BiMenu className="navbar-menu-icon "  onClick={handleNavbar}/>
+      </header>
   )
 }
 
