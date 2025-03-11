99b24f1d0ddcb019264419e9f5453983ab185e15
Bluhawk2004 <161112797+CholarajuAditya@users.noreply.github.com>
2025-03-11 21:07:46 +0530

    changed code to commonJS

--- Back-end/controlers/shop.js
+++ Back-end/controlers/shop.js
@@ -1,9 +1,9 @@
-import { pool } from "../db/connection.js"
+const { pool } = require("../db/connection.js");
 
-const getItems = async(req,res) => {
-    const result = await pool.query("SELECT * from shop;")
-    if(!result) return res.status(500).json({message: "cannot fetch items from shop"})
-    res.status(200).json(result.rows)
-}
+const getItems = async (req, res) => {
+    const result = await pool.query("SELECT * from shop;");
+    if (!result) return res.status(500).json({ message: "cannot fetch items from shop" });
+    res.status(200).json(result.rows);
+};
 
-export {getItems}
+module.exports = { getItems };
